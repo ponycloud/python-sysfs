a55fdba362c079f9426ed51acf6786fc6a487666
Jan Dvořák <mordae@anilinux.org>
2012-08-27 13:10:54 +0200

    added setup.py
    Signed-off-by: Jan Dvořák <mordae@anilinux.org>

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python -tt
+
+from setuptools import setup
+
+setup(
+    name = 'python-sysfs',
+    version = '1',
+    author = 'The PonyCloud Team',
+    description = ('simple sysfs interface'),
+    license = 'proprietary',
+    keywords = 'system',
+    url = 'http://github.com/ponycloud/python-sysfs',
+    packages=['sysfs'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: System',
+        'License :: Other/Proprietary License',
+    ],
+)
+
+
+# vim:set sw=4 ts=4 et:
+# -*- coding: utf-8 -*-
